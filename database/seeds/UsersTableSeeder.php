3a08263d99f029db081864c105100dc294742caf
Guilherme <guips1998@gmail.com>
2020-07-23 11:17:47 -0300

    Relacionamentos

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,13 +11,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('users')->insert([
+//        \DB::table('users')->insert([
+//
+//            'name' => 'administrador',
+//            'email' => 'admin@admin.com',
+//            'email_verified_at' => now(),
+//            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+//            'remember_token' => 'tokenaleatorio',
+//        ]);
 
-            'name' => 'administrador',
-            'email' => 'admin@admin.com',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => 'tokenaleatorio',
-        ]);
+        factory(\App\User::class, 40)->create()->each(function($user){
+            $user->store()->save(factory(\App\Store::class)->make());
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,32 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $helloWorld = 'Hello World';
+
+    return view('welcome', compact('helloWorld'));
+});
+
+Route::get('/model', function (){
+// active record
+//    $user = new \App\User();
+//    $user->name = 'Usuario Teste 2 ';
+//    $user->email = 'email@teste2.com';
+//    $user->password = bcrypt('12345678');
+//    $user->save();
+//
+    //mass created
+//    $user   = \App\User::create([
+//        'name' => 'Guilherme Pereira',
+//        'email' => 'guilherme@email.com',
+//        'password' => bcrypt('12345678')
+//    ]);
+//
+    //mass update
+
+//    $user = \App\User::find(42);
+//    $user->update([
+//       'name' => 'Atualizado com Mass update'
+//     ]);
+    return \App\User::all();
 });
